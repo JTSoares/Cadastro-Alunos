3bd885e84846cceda446ec2b238f872ab6f9febd
João Victor Torres Soares <torresjoao061@gmail.com>
2025-11-03 21:55:46 -0300

    Classe Pessoa, feita para ser usada para posteriores classes ao longo deste programa, como Aluno, Professor e Coordenador

--- src/Model/Pessoa.java
+++ src/Model/Pessoa.java
@@ -1,4 +1,51 @@
 package Model;
 
 public class Pessoa {
+    //**********ATRIBUTOS********
+     private String nome;
+     private String cpf;
+     private int idade;
+
+    //**********CONSTRUTOR********
+    public Pessoa(String nome, int idade, String CPF) {
+        setNome(nome);
+        setIdade(idade);
+        setCPF(CPF);
+    }
+
+    //**********GETTER E SETTER********
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (!nome.isEmpty()) {
+            this.nome = nome;
+        } else if (nome.equals("")) {
+            System.out.println("Nome inválido");
+        }
+    }
+
+    public String getCPF() {
+        return this.cpf;
+    }
+
+    public void setCPF(String cpf) {
+        if ((!cpf.isEmpty()) && (cpf.length() == 11) ) {
+            this.cpf = CPF;
+        }else {
+            System.out.println("CPF INVÁLIDO");
+        }
+
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+            if (idade > 0) {
+                this.idade = idade;
+            }
+    }
 }
